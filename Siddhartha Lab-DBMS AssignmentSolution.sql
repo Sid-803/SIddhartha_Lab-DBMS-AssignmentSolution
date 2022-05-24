0a762a337f12244f2007d84ce403354810b1da7f
Siddhartha Shekhar <84010535+Sid-803@users.noreply.github.com>
2022-05-24 15:05:14 +0530

    Update Siddhartha Lab-DBMS AssignmentSolution.sql

--- Siddhartha Lab-DBMS AssignmentSolution.sql
+++ Siddhartha Lab-DBMS AssignmentSolution.sql
@@ -176,3 +176,27 @@ where orderr.ORD_DATE > '2021-10-05'
 /*8*/
 Select CUS_NAME, CUS_GENDER from customer
 where customer.CUS_NAME LIKE 'A%' or customer.CUS_NAME LIKE '%A'
+
+/*9*/
+DELIMITER &&
+CREATE PROCEDURE proc()
+BEGIN
+Select report.supp_id, report.supp_name,report.Average,
+CASE
+When report.Average = 5 THEN 'Excellent Service'
+When report.Average > 4 THEN 'Good Service'
+When report.Average > 2 THEN 'Average Service'
+ELSE "Poor Service"
+END AS Type_of_Service from
+(SELECT final.supp_id, supplier.supp_name, final.Average from
+(select test2.supp_id, sum(test2.rat_ratstars)/count(test2.rat_ratstars) as average from
+(select supplier_pricing.SUPP_ID, test.ORD_ID, test.RAT_RATSTARS from supplier_pricing inner join
+(select orderr.PRICING_ID, rating.ORD_ID, rating.RAT_RATSTARS
+from orderr inner join rating on rating. ord_id = orderr.ORD_ID
+)as test on test.pricing_id = supplier_pricing.PRICING_ID)
+as test2 group by supplier_pricing.SUPP_ID)
+as final inner join supplier where final.supp_id = supplier.SUPP_ID) as report;
+END &&
+DELIMITER ;
+
+call proc();
